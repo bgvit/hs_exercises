cc4961542583f9ef8671e59903c439d60bab5909
bgvit <bgvit@outlook.com>
2024-09-28 12:36:18 -0300

    created a function to generate palindrome lists like the output proposed in the exercise in real world haskell

---
+++ palindrome.hs
@@ -0,0 +1,10 @@
+splitlist :: [a] -> ([a], [a])
+splitlist xs = splitAt ((length xs + 1) `div` 2) xs
+
+palindrome :: [Integer] -> [Integer]
+palindrome [] = []
+palindrome xs = (xs ++ reverse xs)
+
+-- isPalindrome :: [Integer] -> Bool
+-- isPalindrome [] = False
+-- isPalindrome (x:xs) | odd (length xs) == True = if x == (last xs) then 
