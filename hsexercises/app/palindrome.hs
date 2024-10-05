a89d76140d15059f266014bf4051496a59381d12
bgvit <bgvitor@outlook.com>
2024-10-05 01:54:37 -0300

    added cabal as package system

--- hsexercises/app/palindrome.hs
+++ hsexercises/app/palindrome.hs
@@ -3,8 +3,8 @@ splitlist xs = splitAt ((length xs + 1) `div` 2) xs
 
 palindrome :: [Integer] -> [Integer]
 palindrome [] = []
-palindrome xs = (xs ++ reverse xs)
+palindrome xs = xs ++ reverse xs
 
 -- isPalindrome :: [Integer] -> Bool
 -- isPalindrome [] = False
--- isPalindrome (x:xs) | odd (length xs) == True = if x == (last xs) then 
+-- isPalindrome (x:xs) | odd (length xs) == True = if x == (last xs) then
